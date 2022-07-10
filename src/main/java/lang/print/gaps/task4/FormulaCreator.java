31a27e63ad11060fdfb19119a41c3fa0ccbca4f1
Viktor Glebov <glebov_v94@mail.ru>
2022-07-10 22:24:02 +0400

    Define variables before usage

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -3,7 +3,9 @@ package lang.print.gaps.task4;
 public class FormulaCreator {
     public static void main(String[] args) {
         double a, b, result;
+        a = 1;
+        b = 2;
 
-        result = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2)
+        result = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
